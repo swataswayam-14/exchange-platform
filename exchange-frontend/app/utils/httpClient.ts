0bc462be5a02999fec0e15c284fa749672dacdc4
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-07-24 13:39:57 +0530

    websocket class manager backend logic complete

---
+++ exchange-frontend/app/utils/httpClient.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { Depth, Kline, Ticker, Trade } from "./types";
+
+const BASE_URL = "http://localhost:5000/api/v1";
+
+export async function getTickers(): Promise<Ticker[]>{
+    const response = await axios.get(`${BASE_URL}/tickers`);
+    return response.data;
+}
+
+export async function getTicker(market: string): Promise<Ticker> {
+    const tickers = await getTickers();
+    const ticker = tickers.find(t => t.symbol === market);
+    if(!ticker) {
+        throw new Error(`No Ticker found for ${market}`);
+    }
+    return ticker;
+}
+
+export async function getDepth(market:string):Promise<Depth> {
+    const response = await axios.get(`${BASE_URL}/depth?symbol=${market}`);
+    return response.data;
+}
+
+export async function getTrades(market: string):Promise<Trade[]> {
+    const response = await axios.get(`${BASE_URL}/trades?symbol=${market}`);
+    return response.data;
+}
+
+export async function getKlines(market: string, interval:string, startTime:number, endTime: number): Promise<Kline[]> {
+    const response = await axios.get(`${BASE_URL}/klines?symbol=${market}&interval=${interval}&startTime=${startTime}&endTime=${endTime}`);
+    const data: Kline[] = response.data;
+    return data.sort((x,y) => Number(x.end)< Number(y.end) ? -1: 1);
+}
